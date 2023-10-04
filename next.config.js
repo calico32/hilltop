9513305907c49cb2309ac185bf1e061795142fb4
calico32 <devdoge1@gmail.com>
2023-10-04 09:10:47 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    cpus: 8,
+  },
+}
 
 module.exports = nextConfig
